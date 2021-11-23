1b8eacf2498b5c700f97cc99d91682559611e10b
Gerard Pons <gpguisado@gmail.com>
2021-11-23 13:53:21 +0100

    Nivell 1 - Exercici 6

---
+++ exercici6.php
@@ -0,0 +1,42 @@
+<?php
+
+    function isBitten () {
+        if ((rand() % 2) + 1 == 2) {
+            echo 'charlie is bitten';
+        }
+
+        else {
+            echo 'charlie is not bitten';
+        }
+    }
+
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+    isBitten();
+    echo '<br>';
+
+?>
