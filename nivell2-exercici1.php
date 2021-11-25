fbc0bc0e581a4a730df6272b54bfa997a2ec4bdc
Gerard Pons <gpguisado@gmail.com>
2021-11-25 10:32:14 +0100

    Nivell 2 - Exercici 1

---
+++ nivell2-exercici1.php
@@ -0,0 +1,11 @@
+<?php
+
+// Ens han demanat un llistat de tots els anys on es van produir jocs olímpics
+// desde 1960 inclós fins al 2016. Programa un bucle que calculi i mostri per
+// pantalla els anys olímpics dins de l'interval esmentat.
+
+    for ($year = 1960; $year <= 2016; $year+=4) {
+        echo "L'any olímpic és: $year <br>";
+    }
+
+?>
